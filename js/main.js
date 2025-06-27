1a0aa2e2e54990722c1bb53a9b2343251956007c
jdegaetano <jdegaetano.seo@gmail.com>
2025-06-27 16:31:53 -0300

    Upload de Imágenes

--- js/main.js
+++ js/main.js
@@ -1,28 +1,96 @@
+function categorias(cat) {
 
-// 2. Mostramos estos productos en nuestra página o sección
-let productosContainer = document.getElementById('productos');
+    // Seleccionar el div donde vamos a crear los productos
+    let contenedorProductos = document.getElementById('lista_productos');
+    contenedorProductos.innerHTML = ''; // Limpiar el contenedor antes de agregar nuevos productos
 
-// Crear productos
-let contenedorProducto = document.createElement('div');
+    // Crear Contenedor de Productos  
+    let contenedorProducto = document.createElement('div');
+    
+    // Agregar una clase css al producto
+    contenedorProducto.classList.add('row');
 
-// Agregar una clase css al producto
-contenedorProducto.classList.add('contenedor-producto');
+    let descripcion = '';
 
-// Agregamos un listener que va iterando el listado de productos
-// Se dispara cuando termina la carga del HTML entero
+    switch(cat) {
+        case '1':   
+            descripcion = 'Frutas Frescas';
+            break;
+        case '2':   
+            descripcion = 'Verduras';
+            break;
+        case '3':   
+            descripcion = 'Hortalizas';
+            break;
+        case '5':   
+            descripcion = 'Frutas Exóticas';
+            break;
+        case '4':   
+            descripcion = 'Hierbas Aromáticas';
+            break;                                                                      
+    }
 
 
-document.addEventListener('DOMContentLoaded', (event) => {
+    contenedorProducto.innerHTML += `
+
+        <h3 id="descrip_categoria">${descripcion}</h3>
+        <hr>
+    
+    `
 
     productos.forEach(producto => {
 
-        contenedorProducto.innerHTML += `
-        
-            <div class="card">
-            </div>
-        
-        `
+            if(cat == producto.categoria)
+            {
 
-    })
+            contenedorProducto.innerHTML += `
+            
+                <div class="col-sm-3 mb-4">
 
-})
+                    <!-- Producto -->
+                    <div class="card">
+                        <img src="img/productos/${producto.imagen}" class="card-img-top img-fluid" alt="${producto.nombre}">
+
+                        <div class="card-body">
+                            <div class="row pb-2">
+                                <div class="col-12 text-center">
+                                    <h4 class="card-title">${producto.nombre}</h4>                                
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-sm-12 text-center">
+                                    <p class="card-text">${producto.descripcion.substring(0,45) + '...'}</p>                        
+                                </div>
+                            </div>
+
+                            <div class="row align-items-center text-center mt-3">
+
+                                <div class="col-sm-12 justify-content-end mb-2">
+                                    <h4>${producto.precio}</h4>
+                                </div>                            
+
+                                <div class="col-sm-12 d-flex justify-content-center mb-2">
+                                    <a href="javascript:add(${producto.id});" class="btn bg-verde text-white rounded" id="btn-add">
+                                        SUMAR AL CARRITO
+                                    </a>                                
+                                </div>                            
+
+                            </div>                        
+                            
+                        </div>
+                    </div>
+                    <!-- /Producto -->
+
+                </div> <!-- /col-sm-3 -->
+            
+            `
+           } // Cierre del If
+
+        })
+
+    contenedorProductos.appendChild(contenedorProducto);
+
+} // Cierre Función que filtra por categorias
+
+categorias('1')
